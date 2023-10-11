57a9bec7487d73e597843ae0a1db4cde7e331111
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-10-11 16:21:59 -0700

    Added a print function

--- src/todo-item.js
+++ src/todo-item.js
@@ -6,7 +6,11 @@ const todoFactory = (title, desc, dueDate, priority) => {
     // Format the date before returning it as part of the object
     dueDate = format(dueDate, 'MM-dd-yyyy');
 
-    return {title, desc, dueDate, priority};
+    function print () {
+        return title + ', ' + desc + ', ' + dueDate + ', ' + priority;
+    }
+
+    return {title, desc, dueDate, priority, print};
 };
 
 export default todoFactory;
