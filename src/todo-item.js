8ca0d808de20787a73fba8681d6c04c4382c0d44
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-10-11 15:38:29 -0700

    Added date formatting to factory function

--- src/todo-item.js
+++ src/todo-item.js
@@ -1,5 +1,11 @@
+import format from "date-fns/format/index.js";
+
 // Factory function for a todo item
 const todoFactory = (title, desc, dueDate, priority) => {
+
+    // Format the date before returning it as part of the object
+    dueDate = format(dueDate, 'MM-dd-yyyy');
+
     return {title, desc, dueDate, priority};
 };
 
