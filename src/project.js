9147cb064b3a10d4140da410fc70c8b69d2de85e
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-10-11 16:21:25 -0700

    Created a project factory to hold todo items in

---
+++ src/project.js
@@ -0,0 +1,34 @@
+// Factory function for a project
+const projectFactory = (name) => {
+    // A project holds an array of todo-items
+    const project = new Array();
+
+    // Function to add todo item to project
+    function addTodo (todoItem) {
+        project.push(todoItem);
+    }
+
+    // Function to remove todo item from project
+    function remTodo (todoItem) {
+        for (let i = 0; i < project.length; i++) {
+            if(project[i] == todoItem){
+                project.splice(i, 1);
+            }
+        }
+    }
+
+    function print() {
+        let output = name + '\n';
+
+        project.forEach((todo) => {
+            output += todo.print() + '\n';
+        });
+
+        return output;
+
+    }
+
+    return {name, addTodo, remTodo, print};
+};
+
+export default projectFactory;
