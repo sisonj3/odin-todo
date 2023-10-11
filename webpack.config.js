478ba67005596f0ba7ad7b55b19fb660d96cd1bd
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-10-10 17:11:52 -0700

    Set up initial work directory

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Odin Todo-List',
+    }),
+  ],
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
