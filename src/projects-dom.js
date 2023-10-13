9aa59352490a9c0f866d4f520570868a618cf15d
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-10-12 17:56:49 -0700

    Added index to dataset

--- src/projects-dom.js
+++ src/projects-dom.js
@@ -1,3 +1,5 @@
+import {displayTodos} from './todo-dom';
+
 // Sets up the list of project in the sidebar
 
 // List to hold all projects
@@ -14,10 +16,12 @@ function displayProjects (projectList) {
 
     for (let i = 0; i < projectList.length; i++){
         const projectDom = document.createElement('li');
+
         projectDom.textContent = projectList.getProjectAtIndex(i).name;
+        projectDom.dataset.index = i;
 
         // When clicked display todos from that project
-        projectDom.addEventListener('click', () => console.log(projectDom.textContent + ' was Clicked'));
+        projectDom.addEventListener('click', (e) => displayTodos(projectList.getProjectAtIndex(e.target.dataset.index)));
 
         projectsListDom.appendChild(projectDom);
     }
